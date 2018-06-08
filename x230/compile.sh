e071416928d4c5c17e9e7374da5393e05cb6340a
Tom Hiller <thrilleratplay@gmail.com>
2018-06-08 00:14:46 -0400

    chore(x230): move config copy and make into shared function

--- x230/compile.sh
+++ x230/compile.sh
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: GPL-3.0+
 source "/home/coreboot/common_scripts/variables.sh"
 source "/home/coreboot/common_scripts/download_coreboot.sh"
+source "/home/coreboot/common_scripts/config_and_make.sh"
 
 ################################################################################
 ## MODEL VARIABLES
@@ -16,38 +17,14 @@ MODEL="x230"
 ###############################################
 downloadOrUpdateCoreboot
 
-######################
-##   Copy config   ##
-######################
-if [ ! -f "$DOCKER_COREBOOT_DIR/.config" ]; then
-  echo "Using existing config"
-else
-  if [ -f "$DOCKER_SCRIPT_DIR/config-$COREBOOT_COMMIT" ]; then
-    cp "$DOCKER_SCRIPT_DIR/config-$COREBOOT_COMMIT" "$DOCKER_COREBOOT_DIR/.config"
-    echo "Using config-$COREBOOT_COMMIT"
-  elif [ -f "$DOCKER_SCRIPT_DIR/config-$COREBOOT_TAG" ]; then
-    cp "$DOCKER_SCRIPT_DIR/config-$COREBOOT_TAG" "$DOCKER_COREBOOT_DIR/.config"
-    echo "Using config-$COREBOOT_TAG"
-  else
-    cp "$DOCKER_SCRIPT_DIR/config" "$DOCKER_COREBOOT_DIR/.config"
-    echo "Using default config"
-  fi
-fi
+##############################
+##   Copy config and make   ##
+##############################
+configAndMake
 
-##############
-##   make   ##
-##############
-cd "$DOCKER_COREBOOT_DIR" || exit;
-
-if [ $COREBOOT_CONFIG ]; then
-  make nconfig
-fi
-
-make
-
-###################
-##   Post build  ##
-###################
+#####################
+##   Post build    ##
+#####################
 if [ ! -f "$DOCKER_COREBOOT_DIR/build/coreboot.rom" ]; then
   echo "Uh oh. Things did not go according to plan."
   exit 1;
