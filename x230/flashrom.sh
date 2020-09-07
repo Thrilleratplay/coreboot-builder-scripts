48ca2eb4fe0d81b0a50cff199223659a1c13edf3
Tom Hiller <thrilleratplay@gmail.com>
2020-09-06 21:49:20 -0400

    feat(x230): add basic checks for flashrom.sh

--- x230/flashrom.sh
+++ x230/flashrom.sh
@@ -2,4 +2,14 @@
 # SPDX-License-Identifier: GPL-3.0+
 set -e
 
+if ! command -v flashrom &> /dev/null; then
+    echo "Flashrom cannot be found.  Install and try again."
+    exit 1
+fi
+
+if [ -z "$1" ]; then
+    echo "File name needed.  No argument supplied.";
+    exit 1;
+fi
+
 flashrom -p internal -r "$1.backup" && flashrom -p internal --layout "$PWD/x230/layout.txt" --image bios -w "$1"
