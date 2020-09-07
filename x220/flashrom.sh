2440bbb157e196c39fb81e992c54a146204d95f3
Tom Hiller <thrilleratplay@gmail.com>
2020-09-06 21:49:31 -0400

    feat(x220): add basic checks for flashrom.sh

--- x220/flashrom.sh
+++ x220/flashrom.sh
@@ -2,4 +2,14 @@
 # SPDX-License-Identifier: GPL-3.0+
 set -e
 
+if ! command -v flashrom &> /dev/null; then
+    echo "Flashrom cannot be found.  Install and try again."
+    exit 1
+fi
+
+if [ -z "$1" ]; then
+    echo "File name needed.  No argument supplied.";
+    exit 1;
+fi
+
 flashrom -p internal:laptop=force_I_want_a_brick -r "$1.backup" -c MX25L6405 && flashrom -p internal:laptop=force_I_want_a_brick -w "$1" -c MX25L6405
