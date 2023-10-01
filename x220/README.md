8352da1cb5785acd62ce2a08d4a6eb7473fe02ea
Tom Hiller <thrilleratplay@gmail.com>
2023-09-30 23:44:33 -0400

    chore(x220): update compile script and defconfig for x220

--- x220/README.md
+++ x220/README.md
@@ -1,10 +1,5 @@
 # Lenovo x220/x220 Tablet
 
-### Prep work
-A flashrom binary dump of the stock x220/x220T BIOS is needed to compile Coreboot.
-
-* Place a copy of this dump under the x220/x220T `stock_bios` directory and name it `stock_bios.bin` (to put it another way `./x220/stock_bios/stock_bios.bin`)
-
 ### Compiling
 Build the latest merged into the master git branch:  
 `./build.sh --bleeding-edge x220`
@@ -14,5 +9,5 @@ Latest stable release:
 
 ### Output
 
-`coreboot_lenovo-x220-complete.rom` - The complete Coreboot ROM is the 8MB version used for internal or external flashing.   
+`coreboot_lenovo-x220-complete.rom` - The complete Coreboot ROM is the 8MB version used for internal flashing.   
 `coreboot_lenovo-x220-complete.rom.sha256` - sha256 checksum of 8MB Coreboot Rom
--- x220/compile.sh
+++ x220/compile.sh
@@ -5,15 +5,14 @@
 source /home/coreboot/common_scripts/./variables.sh
 source /home/coreboot/common_scripts/./download_coreboot.sh
 source /home/coreboot/common_scripts/./config_and_make.sh
-
+source /home/coreboot/common_scripts/./extract_stockbios.sh
+source /home/coreboot/common_scripts/./copy_bootsplash.sh
 
 ################################################################################
 ## MODEL VARIABLES
 ################################################################################
 MAINBOARD="lenovo"
 MODEL="x220"
-
-STOCK_BIOS_ROM="stock_bios.bin"
 
 ################################################################################
 
@@ -22,43 +21,16 @@ STOCK_BIOS_ROM="stock_bios.bin"
 ###############################################
 downloadOrUpdateCoreboot
 
-#######################
-##   build ifdtool   ##
-#######################
-if [ ! -f "$DOCKER_COREBOOT_DIR/util/ifdtool/ifdtool" ]; then
-  # Make ifdtool
-  cd "$DOCKER_COREBOOT_DIR/util/ifdtool" || exit
-  make
-  chmod +x ifdtool || exit
-fi
+##############################################################
+##   Export Stock BIOS GBE/ME/IDF (Optional for updating)   ##
+##############################################################
+# uncomment next line to enable
+# extractStockBios "$MAINBOARD" "$MODEL" "stock_bios.bin"
 
-###################################################################################
-##   Extract Intel ME firmware, Gigabit Ethernet firmware and flash descriptor   ##
-###################################################################################
-if [ ! -d "$DOCKER_COREBOOT_DIR/3rdparty/blobs/mainboard/$MAINBOARD/$MODEL/" ]; then
-  mkdir -p "$DOCKER_COREBOOT_DIR/3rdparty/blobs/mainboard/$MAINBOARD/$MODEL/"
-fi
-
-
-if [ ! -f "$DOCKER_COREBOOT_DIR/3rdparty/blobs/mainboard/$MAINBOARD/$MODEL/gbe.bin" ]; then
-  cd "$DOCKER_COREBOOT_DIR/3rdparty/blobs/mainboard/$MAINBOARD/$MODEL/" || exit
-
-  cp "$DOCKER_COREBOOT_DIR/util/ifdtool/ifdtool" .
-
-  # ALWAYS COPY THE ORIGINAL.  Never modified the original stock bios file
-  cp "$DOCKER_STOCK_BIOS_DIR/$STOCK_BIOS_ROM" .
-
-  # unlock, extract blobs and rename
-  ./ifdtool -u "$STOCK_BIOS_ROM" || exit
-  ./ifdtool -x "$STOCK_BIOS_ROM" || exit
-  mv flashregion_0_flashdescriptor.bin descriptor.bin
-  mv flashregion_2_intel_me.bin me.bin
-  mv flashregion_3_gbe.bin gbe.bin
-
-  # clean up
-  rm ifdtool
-  rm flashregion_1_bios.bin
-fi
+#############################
+##   Copy bootsplash.jpg   ##
+#############################
+copySeaBiosBootsplash
 
 ##############################
 ##   Copy config and make   ##
