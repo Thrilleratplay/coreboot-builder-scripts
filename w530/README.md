c3f255e64de1bb0059330500940f5e9cfae59dd1
Tom Hiller <thrilleratplay@gmail.com>
2023-10-08 18:45:22 -0400

    feat(w530): initial add of w530

---
+++ w530/README.md
@@ -0,0 +1,22 @@
+# Lenovo W530
+
+### Compiling
+Build the latest merged into the master git branch:  
+`./build.sh --bleeding-edge w530`
+
+Latest stable release:  
+ `./build.sh w530`
+
+### Output
+ ##### Internal flashing
+`coreboot_lenovo-w530-complete.rom` - The complete Coreboot ROM is the 12MB version used for internal flashing.   
+`coreboot_lenovo-w530-complete.rom.sha256` - sha256 checksum of 12MB Coreboot Rom
+
+*NOTE:* As this is compiled without the stock BIOS, all IFD, GBE and ME blobs are stubs.  Use the `flash.sh` script at the root of the directory.
+
+
+##### External flashing
+`coreboot_lenovo-w530-top.rom` - The 4MB Coreboot BIOS that can be flashed externally onto the top BIOS chip.
+`coreboot_lenovo-w530-top.rom.sha256` - sha256 checksum of 4MB Coreboot BIOS
+
+*NOTE:* See [Skulls](https://github.com/merge/skulls/tree/master/w530) for instructions on how to flash the top BIOS chip as well as unlocking the IFD and cleaning Intel ME
---
+++ w530/compile.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0+
+
+# shellcheck disable=SC1091
+source /home/coreboot/common_scripts/./variables.sh
+source /home/coreboot/common_scripts/./download_coreboot.sh
+source /home/coreboot/common_scripts/./config_and_make.sh
+source /home/coreboot/common_scripts/./extract_stockbios.sh
+source /home/coreboot/common_scripts/./copy_bootsplash.sh
+
+################################################################################
+## MODEL VARIABLES
+################################################################################
+MAINBOARD="lenovo"
+MODEL="w530"
+
+################################################################################
+
+###############################################
+##   download/git clone/git pull Coreboot    ##
+###############################################
+downloadOrUpdateCoreboot
+
+##############################################################
+##   Export Stock BIOS GBE/ME/IDF (Optional for updating)   ##
+##############################################################
+# uncomment next line to enable
+# extractStockBios "$MAINBOARD" "$MODEL" "stock_bios.bin"
+
+#############################
+##   Copy bootsplash.jpg   ##
+#############################
+copySeaBiosBootsplash
+
+##############################
+##   Copy config and make   ##
+##############################
+configAndMake
+
+#####################
+##   Post build    ##
+#####################
+if [ ! -f "$DOCKER_COREBOOT_DIR/build/coreboot.rom" ]; then
+  echo "Uh oh. Things did not go according to plan."
+  exit 1;
+else
+  mv "$DOCKER_COREBOOT_DIR/build/coreboot.rom" "$DOCKER_COREBOOT_DIR/coreboot_$MAINBOARD-$MODEL-complete.rom"
+
+  #split out top BIOS
+  dd if="$DOCKER_COREBOOT_DIR/coreboot_$MAINBOARD-$MODEL-complete.rom" of="$DOCKER_COREBOOT_DIR/coreboot_$MAINBOARD-$MODEL-top.rom" bs=1M skip=8
+
+  sha256sum "$DOCKER_COREBOOT_DIR/coreboot_$MAINBOARD-$MODEL-complete.rom" > "$DOCKER_COREBOOT_DIR/coreboot_$MAINBOARD-$MODEL-complete.rom.sha256"
+  sha256sum "$DOCKER_COREBOOT_DIR/coreboot_$MAINBOARD-$MODEL-top.rom" > "$DOCKER_COREBOOT_DIR/coreboot_$MAINBOARD-$MODEL-top.rom-sha256"
+fi
