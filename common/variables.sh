31b74e6402862417fc94045e529d3f65dcc24a81
Tom Hiller <thrilleratplay@gmail.com>
2024-12-24 16:06:41 -0500

    chore(variables):update SDK

--- common/variables.sh
+++ common/variables.sh
@@ -4,7 +4,7 @@
 ################################################################################
 ## VARIABLES
 ################################################################################
-export COREBOOT_SDK_VERSION="2023-06-04_44f676afc9"
+export COREBOOT_SDK_VERSION="2024-12-21_306660c2de"
 
 export DOCKER_ROOT_DIR="/home/coreboot"
 export DOCKER_SCRIPT_DIR="$DOCKER_ROOT_DIR/scripts"
