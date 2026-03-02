5e5045c9b990b51e54021d9a7d77138456bf5c3b
Tom Hiller <thrilleratplay@gmail.com>
2026-03-02 18:02:47 -0500

    feat: add m720q

---
+++ m720q/compile.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0+
+
+# shellcheck disable=SC1091
+source /home/coreboot/common_scripts/./variables.sh
+source /home/coreboot/common_scripts/./download_coreboot.sh
+source /home/coreboot/common_scripts/./config_and_make.sh
+source /home/coreboot/common_scripts/./extract_stockbios.sh
+source /home/coreboot/common_scripts/./copy_bootsplash.sh
+
+################################################################################
+## MODEL VARIABLES
+################################################################################
+MAINBOARD="lenovo"
+MODEL="m720q"
+
+################################################################################
+
+###############################################
+##   download/git clone/git pull Coreboot    ##
+###############################################
+downloadOrUpdateCoreboot
+
+##############################################################
+##   Export Stock BIOS GBE/ME/IDF (Optional for updating)   ##
+##############################################################
+# uncomment next line to enable
+# extractStockBios "$MAINBOARD" "m720q_m920q" "stock_bios.bin" "sklkbl"
+
+#############################
+##   Copy bootsplash.jpg   ##
+#############################
+copySeaBiosBootsplash
+
+##############################
+##   Copy config and make   ##
+##############################
+configAndMake
+
+#####################
+##   Post build    ##
+#####################
+if [ ! -f "$DOCKER_COREBOOT_DIR/build/coreboot.rom" ]; then
+  echo "Uh oh. Things did not go according to plan."
+  exit 1;
+else
+  mv "$DOCKER_COREBOOT_DIR/build/coreboot.rom" "$DOCKER_COREBOOT_DIR/coreboot_$MAINBOARD-$MODEL-complete.rom"
+  
+  sha256sum "$DOCKER_COREBOOT_DIR/coreboot_$MAINBOARD-$MODEL-complete.rom" > "$DOCKER_COREBOOT_DIR/coreboot_$MAINBOARD-$MODEL-complete.rom.sha256"
+fi
